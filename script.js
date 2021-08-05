9154afa1465050f85f0d68e37e80d9cd4bceb300
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-05 17:28:35 +0300

    add changes

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-function validate(){
-    var n=document.getElementById("n1").value;  
-    var e=document.getElementById("e2").value;  
-    var y=document.getElementById("y3").value;  
-    var p=document.getElementById("p4").value;
-    var m=document.getElementById("m5").value;
+function validate() {
+    var n = document.getElementById("n1").value;
+    var e = document.getElementById("e2").value;
+    var y = document.getElementById("y3").value;
+    var p = document.getElementById("p4").value;
+    var m = document.getElementById("m5").value;
 
     //validation required before submiting form
-if(n1.value==''){
-    alert("Please enter Name of the product");
-    n1.focus();
-    return false;
-}
-if(e2.value==''){
-    alert("Please enter your email");
-    e2.focus();
-    return false; 
+    if (n1.value == '') {
+        alert("Please enter Name of the product");
+        n1.focus();
+        return false;
+    }
+    if (e2.value == '') {
+        alert("Please enter your email");
+        e2.focus();
+        return false;
 
-}
-if(y3.value==''){
-    alert("Please enter your number");
-    y3.focus();
-    return false;
-}
-if(p4.value==''){
-    alert("Please enter your Category");
-    p4.focus();
-    return false;
-}
-if(m5.value==''){
-    alert("Please enter your product description");
-    m5.focus();
-    return false;
-}
-else{
-    alert("Your message has been sent");
-    return true;
-};
+    }
+    if (y3.value == '') {
+        alert("Please enter your number");
+        y3.focus();
+        return false;
+    }
+    if (p4.value == '') {
+        alert("Please enter your Category");
+        p4.focus();
+        return false;
+    }
+    if (m5.value == '') {
+        alert("Please enter your product description");
+        m5.focus();
+        return false;
+    } else {
+        alert("Your message has been sent");
+        return true;
+    };
 };
 
 // $(document).ready(function () {
@@ -67,10 +66,63 @@ else{
 function submitForm() {
     $('contactForm[name="contactForm"]').submit();
     $('input[type="text"], textarea').val('');
-  }
-$("#but").click(function (event) {
+}
+$("#but").click(function(event) {
     event.preventDefault();
     alert("Thank you for your message, we'll get back to you as soon as we can")
+    $("#deliver").show();
+
+});
+
+
+
+function validate() {
+    var n = document.getElementById("kk1").value;
+    var e = document.getElementById("k2").value;
+    var y = document.getElementById("k3").value;
+    var p = document.getElementById("k4").value;
+    var m = document.getElementById("k5").value;
+
+    //validation required before submiting form
+    if (kk1.value == '') {
+        alert("Please enter Name of the product");
+        k1.focus();
+        return false;
+    }
+    if (k2.value == '') {
+        alert("Please enter your Price");
+        k2.focus();
+        return false;
+
+    }
+    if (k3.value == '') {
+        alert("Please enter your Category");
+        k3.focus();
+        return false;
+
+    }
+    if (k4.value == '') {
+        alert("Please enter your Title");
+        k4.focus();
+        return false;
+    }
+    if (k5.value == '') {
+        alert("Please enter your product description");
+        k5.focus();
+        return false;
+    } else {
+        alert("Your message has been sent");
+        return true;
+    };
+};
+
+function submitForm() {
+    $('contactForm[name="contactForm"]').submit();
+    $('input[type="text"], textarea').val('');
+}
+$("#but").click(function(event) {
+    event.preventDefault();
+    alert("We have a customer for your goods, we'll share the contact")
     $("#deliver").show();
 
 });
