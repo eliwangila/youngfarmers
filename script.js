3ea486b613a2d8bfe9cac1c55d541fd24f438b07
eliwangila <ekirapaeli254@gmail.com>
2021-08-06 08:14:31 +0300

    modified

--- script.js
+++ script.js
@@ -37,31 +37,6 @@ function validate() {
     };
 };
 
-// $(document).ready(function () {
-//     var isFormValid = true;
-
-//     function checkFormValidity(form){
-//         isFormValid = true;
-//         $(form).find(".check-validity").each(function(){
-//             var fieldVal = $.trim($(this).val());
-//             if(!fieldVal){
-//                 isFormValid = false;
-//             }
-//         });
-//     }
-
-
-//     //option A
-//     $("#form").submit(function (e) {
-//         checkFormValidity("#form");
-//         if(isForm === ""){
-//             alert("Submit Form Submitted!!! :D");
-//         }else{
-//             alert("Form is not valid :(");
-//         }
-//         e.preventDefault();
-//     });
-// });
 
 function submitForm() {
     $('contactForm[name="contactForm"]').submit();
@@ -76,53 +51,42 @@ $("#but").click(function(event) {
 
 
 
-function validate() {
-    var n = document.getElementById("kk1").value;
-    var e = document.getElementById("k2").value;
-    var y = document.getElementById("k3").value;
-    var p = document.getElementById("k4").value;
-    var m = document.getElementById("k5").value;
+// function validate() {
+//     var n = document.getElementById("kk1").value;
+//     var e = document.getElementById("k2").value;
+//     var y = document.getElementById("k3").value;
+//     var p = document.getElementById("k4").value;
+//     var m = document.getElementById("k5").value;
 
-    //validation required before submiting form
-    if (kk1.value == '') {
-        alert("Please enter Name of the product");
-        k1.focus();
-        return false;
-    }
-    if (k2.value == '') {
-        alert("Please enter your Price");
-        k2.focus();
-        return false;
+//     //validation required before submiting form
+//     if (kk1.value == '') {
+//         alert("Please enter Name of the product");
+//         k1.focus();
+//         return false;
+//     }
+//     if (k2.value == '') {
+//         alert("Please enter your Price");
+//         k2.focus();
+//         return false;
 
-    }
-    if (k3.value == '') {
-        alert("Please enter your Category");
-        k3.focus();
-        return false;
+//     }
+//     if (k3.value == '') {
+//         alert("Please enter your Category");
+//         k3.focus();
+//         return false;
 
-    }
-    if (k4.value == '') {
-        alert("Please enter your Title");
-        k4.focus();
-        return false;
-    }
-    if (k5.value == '') {
-        alert("Please enter your product description");
-        k5.focus();
-        return false;
-    } else {
-        alert("Your message has been sent");
-        return true;
-    };
-};
-
-function submitForm() {
-    $('contactForm[name="contactForm"]').submit();
-    $('input[type="text"], textarea').val('');
-}
-$("#but").click(function(event) {
-    event.preventDefault();
-    alert("We have a customer for your goods, we'll share the contact")
-    $("#deliver").show();
-
-});
+//     }
+//     if (k4.value == '') {
+//         alert("Please enter your Title");
+//         k4.focus();
+//         return false;
+//     }
+//     if (k5.value == '') {
+//         alert("Please enter your product description");
+//         k5.focus();
+//         return false;
+//     } else {
+//         alert("Your message has been sent");
+//         return true;
+//     };
+// };
